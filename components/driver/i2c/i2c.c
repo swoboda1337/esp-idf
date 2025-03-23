8b4491df03c963ec120ed5bd2e4ba676956c4487
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-03-23 15:43:36 -0400

    Add defines

--- components/driver/i2c/i2c.c
+++ components/driver/i2c/i2c.c
@@ -68,6 +68,7 @@ static const char *I2C_TAG = "i2c";
 #define I2C_CMD_USER_ALLOC_ERR_STR     "i2c command link allocation error: the buffer provided is too small."
 #define I2C_TRANS_MODE_ERR_STR         "i2c trans mode error"
 #define I2C_MODE_ERR_STR               "i2c mode error"
+#define I2C_CLEAR_BUS_FAILED_STR       "clear bus failed"
 #define I2C_SDA_IO_ERR_STR             "sda gpio number error"
 #define I2C_SCL_IO_ERR_STR             "scl gpio number error"
 #define I2C_SCL_SDA_EQUAL_ERR_STR      "scl and sda gpio numbers are the same"
@@ -125,6 +126,8 @@ static const char *I2C_TAG = "i2c";
 #else
 #define I2C_RCC_ATOMIC()
 #endif
+
+#define I2C_CLR_BUS_TIMEOUT_MS        (50)  // 50ms is sufficient for clearing the bus
 
 /**
  * I2C bus are defined in the header files, let's check that the values are correct
@@ -657,6 +660,7 @@ esp_err_t i2c_get_data_mode(i2c_port_t i2c_num, i2c_trans_mode_t *tx_trans_mode,
  **/
 static esp_err_t i2c_master_clear_bus(i2c_port_t i2c_num)
 {
+    esp_err_t ret = ESP_OK;
 #if !SOC_I2C_SUPPORT_HW_CLR_BUS
     const int scl_half_period = I2C_CLR_BUS_HALF_PERIOD_US; // use standard 100kHz data rate
     int i = 0;
@@ -692,7 +696,7 @@ static esp_err_t i2c_master_clear_bus(i2c_port_t i2c_num)
     const TickType_t timeout_ticks = pdMS_TO_TICKS(I2C_CLR_BUS_TIMEOUT_MS);
     while (i2c_ll_master_is_bus_clear_done(i2c_context[i2c_num].hal.dev)) {
         if ((xTaskGetTickCount() - start_tick) > timeout_ticks) {
-            ESP_LOGE(TAG, "clear bus failed.");
+            ESP_LOGE(I2C_TAG, I2C_CLEAR_BUS_FAILED_STR);
             i2c_ll_master_clr_bus(i2c_context[i2c_num].hal.dev, 0, false);
             ret = ESP_ERR_INVALID_STATE;
             break;
@@ -700,7 +704,7 @@ static esp_err_t i2c_master_clear_bus(i2c_port_t i2c_num)
     }
     i2c_ll_update(i2c_context[i2c_num].hal.dev);
 #endif
-    return ESP_OK;
+    return ret;
 }
 
 /**if the power and SDA/SCL wires are in proper condition, everything works find with reading the slave.
